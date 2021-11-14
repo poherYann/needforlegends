f490a09b7bd749505e5f2598a493e92ac9bd6952
Julien76140 <julien.mallet.pro@gmail.com>
2021-11-14 23:24:25 +0100

    fix router link + tooaster in logout service

--- src/app/Service/logout.service.ts
+++ src/app/Service/logout.service.ts
@@ -1,16 +1,18 @@
 import { Injectable } from '@angular/core';
 import {Router} from "@angular/router";
+import {ToastrService} from "ngx-toastr";
 
 @Injectable({
   providedIn: 'root'
 })
 export class LogoutService {
 
-  constructor(private router:Router) { }
+  constructor(private router:Router,private toaster:ToastrService) { }
 
 
   logout(){
     localStorage.clear();
+    this.toaster.success("Vous avez bien été déconnecté !");
     this.router.navigateByUrl("/home");
   }
 }
