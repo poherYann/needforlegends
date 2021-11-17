f2c3540ac0f0cb818c831b4a59cdba4d9ea088b7
Julien76140 <julien.mallet.pro@gmail.com>
2021-11-17 17:51:56 +0100

    match detail button ok

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -62,7 +62,7 @@ const appRoutes: Routes = [
     MatchComponent,
     SearchComponent,
     AlgorithmeComponent,
-    LogoutComponent
+    LogoutComponent,
   ],
   imports: [
     BrowserModule,
--- src/app/match/match.component.ts
+++ src/app/match/match.component.ts
@@ -1,7 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, ElementRef, OnInit, Renderer2} from '@angular/core';
 import {ActivatedRoute} from "@angular/router";
 import {MatchService} from "../service/match.service";
 import {GetSummonerResolver} from "../Service/resolver.service";
+import {match} from "../match";
+import {interval} from "rxjs";
 
 @Component({
   selector: 'app-match',
@@ -14,8 +16,9 @@ export class MatchComponent implements OnInit {
   public arrayChampions:any;
   public arrayItems:any;
   public arraySummoners:any;
+  public arrayParticipant:any;
 
-  constructor(private route: ActivatedRoute,private matchService:MatchService) {
+  constructor(private route: ActivatedRoute,private matchService:MatchService,private renderer:Renderer2) {
 
   }
 
@@ -37,5 +40,90 @@ export class MatchComponent implements OnInit {
 
       });
   }
+  match_detail(matchId:string){
 
+    let match_detail=document.getElementById(matchId);
+
+    if(match_detail!=null) {
+
+      match_detail.style.display = "block";
+
+      let divMatch=document.createElement("div");
+      divMatch.setAttribute("class","match");
+
+      for(let i=0;i<this.arrayMatches['matchs'].length;i++){
+        if(this.arrayMatches['matchs'][i]["match_id"]===matchId){
+          let participants=this.arrayMatches['matchs'][i]["participant"];
+          for(let v=0;v<participants.length;v++){
+
+            let divMatchLeft=document.createElement("div");
+            divMatchLeft.setAttribute("class","divMatchLeft");
+            divMatch.appendChild(divMatchLeft);
+            let divMatchRight=document.createElement("div");
+            divMatchRight.setAttribute("class","divMatchRight");
+            divMatch.appendChild(divMatchRight);
+
+            for(let u=0;u<this.arrayChampions.length;u++){
+
+              if(this.arrayChampions[u]['KeyChampion']===participants[v]['championId']){
+                let imgChampion=document.createElement("img");
+                imgChampion.setAttribute("src","//opgg-static.akamaized.net/images/lol/champion/"+this.arrayChampions[u]['Image']['full']+"?image=c_scale,q_auto,w_46&v=1635906101");
+                divMatchLeft.appendChild(imgChampion);
+                break;
+              }
+            }
+
+            let spellDiv =document.createElement('div');
+            spellDiv.setAttribute("class","spell");
+            divMatchLeft.appendChild(spellDiv);
+
+            for(let u=0;u<this.arraySummoners.length;u++){
+
+              if(this.arraySummoners[u]['keySummoners']==participants[v]['spell1Id'] || this.arraySummoners[u]['keySummoners']==participants[v]['spell2Id'] ){
+                let imgSummoners=document.createElement("img");
+                imgSummoners.setAttribute("src","//opgg-static.akamaized.net/images/lol/spell/"+this.arraySummoners[u]['image']+"?image=c_scale,q_auto,w_46&v=1635906101");
+                spellDiv.appendChild(imgSummoners);
+              }
+            }
+            let runesDiv =document.createElement('div');
+            runesDiv.setAttribute("class","runes");
+            let imgPerk0=document.createElement("img");
+            imgPerk0.setAttribute("src","//opgg-static.akamaized.net/images/lol/perk/"+participants[v]['perk0']+".png?image=c_scale,q_auto,w_46&v=1635906101");
+            let imgPerk5=document.createElement("img");
+            imgPerk5.setAttribute("src","//opgg-static.akamaized.net/images/lol/perk/"+participants[v]['perk5']+".png?image=c_scale,q_auto,w_46&v=1635906101");
+            divMatchLeft.appendChild(runesDiv);
+            runesDiv.appendChild(imgPerk0);
+            runesDiv.appendChild(imgPerk5);
+            let divText =document.createElement('div');
+            divText.setAttribute("class","text");
+            divMatchRight.appendChild(divText);
+            let peleme =document.createElement('p');
+            peleme.innerHTML=participants[v]['kills']+"/"+participants[v]['deaths']+"/"+participants[v]['assist'];
+            divText.appendChild(peleme);
+            let divImgItems =document.createElement('div');
+            divImgItems.setAttribute("class","imgs");
+            divText.appendChild(divImgItems);
+
+            for(let p=0;p<participants[v]["item"].length;p++){
+              let imgItem=document.createElement('img');
+              imgItem.setAttribute("src","https://opgg-static.akamaized.net/images/lol/item/"+participants[v]["item"][p]+".png?image=c_scale,q_auto,w_22&v=1635906101");
+              divImgItems.appendChild(imgItem);
+            }
+
+            let divText2=document.createElement('div');
+            divText2.setAttribute("class","text");
+            divText.appendChild(divText2);
+
+            let peleme2 =document.createElement('p');
+            peleme2.innerHTML=((participants[v]['kills']+participants[v]['assist'])/participants[v]['deaths']).toFixed(2)+" KDA";
+            divText2.appendChild(peleme2);
+
+          }
+          break;
+        }
+      }
+      match_detail.appendChild(divMatch);
+
+    }
+  }
 }
