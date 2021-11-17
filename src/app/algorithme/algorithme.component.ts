d0a2e068423f8a48139e94fe7eff9043d5569c6b
Julien76140 <julien.mallet.pro@gmail.com>
2021-11-17 16:01:46 +0100

    algo current

--- src/app/algorithme/algorithme.component.ts
+++ src/app/algorithme/algorithme.component.ts
@@ -18,6 +18,14 @@ export class AlgorithmeComponent implements OnInit {
   private lose=0;
   private minion=0;
   private firstKill=0;
+  private baronKill=0;
+  private goldEarned=0;
+  private firstTowerKill=0;
+  private scoreUser=[];
+  private arrayAggressive=[];
+  private arrayObjective=[];
+  private arrayPassive=[];
+
 
   constructor(private route:ActivatedRoute) { }
 
@@ -26,42 +34,106 @@ export class AlgorithmeComponent implements OnInit {
     this.route.data.subscribe(value =>{
 
         this.arrayMatchStat=value;
-        this.numberMatch=this.arrayMatchStat['data']['matchs'].length;
 
-        for(let k=0;k<this.arrayMatchStat['data']['matchs'].length;k++){
-          console.log(this.arrayMatchStat['data']['matchs'][k]);
-          this.kills+=this.arrayMatchStat['data']['matchs'][k]["user"][0]['userStat']['kills'];
-          this.deaths+=this.arrayMatchStat['data']['matchs'][k]["user"][0]['userStat']['deaths'];
-          this.assists+=this.arrayMatchStat['data']['matchs'][k]["user"][0]['userStat']['assist'];
-          this.totalDamage+=this.arrayMatchStat['data']['matchs'][k]["user"][0]['userStat']['totalDamageDealtToChampion'];
+        for(let k=1;k<this.arrayMatchStat['data']['matchs'].length;k++){
 
-          if(this.arrayMatchStat['data']['matchs'][k]["user"][0]['userParticipant']['win']==="Fail"){
-            this.win++;
-          }else{
-            this.lose++;
+          if(this.arrayMatchStat['data']['matchs'][k]['summoner'][0][0]==this.arrayMatchStat['data']['matchs'][k-1]['summoner'][0][0]) {
+            this.numberMatch=this.arrayMatchStat['data']['matchs'].length;
+
+            this.kills += this.arrayMatchStat['data']['matchs'][k]["user"][0]['userStat']['kills'];
+            this.deaths += this.arrayMatchStat['data']['matchs'][k]["user"][0]['userStat']['deaths'];
+            this.assists += this.arrayMatchStat['data']['matchs'][k]["user"][0]['userStat']['assist'];
+            this.totalDamage += this.arrayMatchStat['data']['matchs'][k]["user"][0]['userStat']['totalDamageDealtToChampion'];
+            this.baronKill += this.arrayMatchStat['data']['matchs'][k]["user"][0]['userParticipant']['baronKills'];
+            this.goldEarned += this.arrayMatchStat['data']['matchs'][k]["user"][0]['userStat']['goldEarned'];
+
+            if (this.arrayMatchStat['data']['matchs'][k]["user"][0]['userParticipant']['win'] === "Fail") {
+              this.win++;
+            } else {
+              this.lose++;
+            }
+
+            this.minion += this.arrayMatchStat['data']['matchs'][k]["user"][0]['userStat']['totalMinionsKilled'];
+
+            if (this.arrayMatchStat['data']['matchs'][k]["user"][0]['userStat']['firstBloodKill'] === true) {
+              this.firstKill++;
+            }
+            if (this.arrayMatchStat['data']['matchs'][k]["user"][0]['userStat']['firstTowerKill'] === true) {
+              this.firstTowerKill++;
+            }
+
           }
+          if(k==this.arrayMatchStat['data']['matchs'].length-1 || this.arrayMatchStat['data']['matchs'][k]['summoner'][0][0]!=this.arrayMatchStat['data']['matchs'][k-1]['summoner'][0][0]){
 
-          this.minion+=this.arrayMatchStat['data']['matchs'][k]["user"][0]['userStat']['totalMinionsKilled'];
-
-          if(this.arrayMatchStat['data']['matchs'][k]["user"][0]['userStat']['firstBloodKill']===true){
-            this.firstKill++;
+            let array={"summoner":this.arrayMatchStat['data']['matchs'][k-1]['summoner'][0][0],"data":[(this.kills+this.assists)/this.numberMatch,(this.firstKill/this.numberMatch)*100,this.totalDamage/this.numberMatch,
+            this.goldEarned/this.numberMatch,(this.firstTowerKill/this.numberMatch)*100,this.arrayMatchStat['data']['matchs'][k-1]['summoner'][0][0]]};
+            this.resetValue();
+            // @ts-ignore
+            this.scoreUser.push(array);
           }
-
         }
-        console.log("death/game "+ this.deaths/this.numberMatch);
-        console.log("kills/game  "+this.kills/this.numberMatch);
-        console.log("assist/game  "+this.assists/this.numberMatch);
-        console.log("total/game  "+this.totalDamage/this.numberMatch);
-        console.log("lose/game  "+this.lose);
-        console.log("win/game  "+this.win);
-        console.log("minion/game  "+this.minion/this.numberMatch);
-        console.log("first blood/game  "+(this.firstKill/this.numberMatch)*100+"%");
+        console.log(this.scoreUser);
+        this.AggressiveScore(this.scoreUser);
 
       },
       (error)=>{
         console.log(error);
 
       });
+
+  }
+
+  AggressiveScore(valueArray:any){
+
+    for (let j=0;j<valueArray.length-1;j++) {
+      // @ts-ignore
+      let swap = true;
+
+      while (swap) {
+        swap = false;
+        for (let i = 0; i < this.scoreUser.length - 1; i++) {
+          // @ts-ignore
+          for (let v=0;v<this.scoreUser[i]['data'].length;v++) {
+
+            if (this.scoreUser[i]['data'][v] > this.scoreUser[i + 1]['data'][v]) {
+              let item = this.scoreUser[i]['data'][v];
+              this.scoreUser[i]['data'][v] = this.scoreUser[i + 1]['data'][v];
+              this.scoreUser[i + 1]['data'][v] = item;
+              swap = true;
+            }
+          }
+        }
+      }
+      this.setScore();
+    }
+  }
+
+  setScore(){
+
+
+    /*if(this.arrayAggressive[0]==null) {
+      for (let u = 0; u < this.scoreUser.length; u++) {
+        // @ts-ignore
+        this.arrayAggressive.push({"summoner": this.scoreUser[u]['summoner'], "score": u});
+      }
+    }*/
+  }
+
+  resetValue(){
+
+    this.kills=0;
+    this.deaths=0;
+    this.assists=0;
+    this.numberMatch=0;
+    this.totalDamage=0;
+    this.win=0;
+    this.lose=0;
+    this.minion=0;
+    this.firstKill=0;
+    this.baronKill=0;
+    this.goldEarned=0;
+    this.firstTowerKill=0;
+
   }
 
 }
